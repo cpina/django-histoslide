837b44c669cb0a6356211152939c637343393fe0
Carles Pina i Estany <carles@pina.cat>
2020-12-08 22:20:06 +0000

    move histoslide app into the correct directory and add it into settings.py

--- slide_visualizer/slide_visualizer/urls.py
+++ slide_visualizer/slide_visualizer/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('main.urls')),
+    path('histoslide/', include('histoslide.urls')),
     path('admin/', admin.site.urls),
 ]
